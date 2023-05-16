51f50e68991a3a8a5e383eda0497d58287bf2722
Ryuichi Takashima <ryuichi.takashima@gmail.com>
2023-05-16 20:02:15 +0900

    Revert "Section 9 (list 9.38)"

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,24 +2,12 @@ module SessionsHelper
   # 渡されたユーザーでログインする
   def log_in(user)
     session[:user_id] = user.id
-    # セッションリプレイ攻撃から保護する
-    # 詳しくは https://bit.ly/33UvK0w を参照
-    session[:session_token] = user.session_token
   end
 
   # 現在ログイン中のユーザーを返す（いる場合）
   def current_user
-    if (user_id = session[:user_id])
-      user = User.find_by(id: user_id)
-      if user && session[:session_token] == user.session_token
-        @current_user = user
-      end
-    elsif (user_id = cookies.encrypted[:user_id])
-      user = User.find_by(id: user_id)
-      if user && user.authenticated?(cookies[:remember_token])
-        log_in user
-        @current_user = user
-      end
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
     end
   end
 
